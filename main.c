db72fe7fa33dab2802938c0d4131922d2a43c883
Aaron <aaronjau21@gmail.com>
2022-12-20 12:48:51 -0500

    push, pull

--- main.c
+++ main.c
@@ -1,53 +1,97 @@
 #include "monty.h"
-stack_t *head = NULL;
+
+global_t global = {NULL, NULL, NULL, NULL, 1, -1, 1};
 
 /**
- * main - Entry Point
- * @argc: Number of command line arguments.
- * @argv: An array containing the arguments.
- * Return: Always Zero.
+ * choose_option - Find if the flag match with an existence and
+ * give the correct function.
+ *
+ * @tokens: Double pointer to the tokens of the command.
+ *
+ * Return: A print_operation struct with a flag (0 or 1
+ * and a function pointer or NULL.
  */
-int main(int argc, char **argv)
+void (*choose_option(char **tokens))(stack_t **, unsigned int)
 {
-	if (argc < 2 || argc > 2)
-		err(1);
-	open_file(argv[1]);
-	free_nodes();
-	return (0);
-}
+	instruction_t options[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"nop", nop},
+		{"swap", swap},
+		{"add", add},
+		{"sub", sub},
+		{"div", _div},
+		{"mul", mul},
+		{"mod", mod},
+		{"pchar", pchar},
+		{"pstr", pstr},
+		{"rotl", rotl},
+		{"rotr", rotr},
+		{"stack", stack},
+		{"queue", queue},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-/**
- * free_nodes - Frees nodes in the stack.
- */
-void free_nodes(void)
-{
-	stack_t *tmp;
-
-	if (head == NULL)
-		return;
-
-	while (head != NULL)
+	while (options[i].opcode)
 	{
-		tmp = head;
-		head = head->next;
-		free(tmp);
+		if (strcmp(options[i].opcode, tokens[0]) == 0)
+			return (options[i].f);
+		i++;
 	}
+	return (NULL);
 }
 
 /**
- * create_node - Creates and populates a node.
- * @n: Number to go inside the node.
- * Return: Upon sucess a pointer to the node. Otherwise NULL.
+ * main - Function that calls opcodes
+ *
+ * @ac: Arguments Count
+ * @av: Arguments Vector
+ * Return: int
  */
-stack_t *create_node(int n)
-{
-	stack_t *node;
 
-	node = malloc(sizeof(stack_t));
-	if (node == NULL)
-		err(4);
-	node->next = NULL;
-	node->prev = NULL;
-	node->n = n;
-    return (node);
+int main(int ac, char **av)
+{
+	size_t len = 0;
+	void (*op_func)(stack_t **, unsigned int);
+
+	if (ac != 2)
+		dprintf(STDERR_FILENO, "USAGE: monty file\n"), exit(EXIT_FAILURE);
+
+	if (access(av[1], R_OK) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", av[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	global.fd_monty = fopen(av[1], "r");
+	while (getline(&global.command, &len, global.fd_monty) != EOF)
+	{
+		if (global.command[0] == '\n' || is_comment(global.command) ||
+			all_spaces(global.command, strlen(global.command)))
+		{
+			global.line_num++, len = 0, free(global.command);
+			continue;
+		}
+		global.tokens = tokenizer(global.command);
+		if (!global.tokens)
+		{
+			dprintf(STDERR_FILENO, "Error: malloc failed\n");
+			free_cases(1);
+			exit(EXIT_FAILURE);
+		}
+		op_func = choose_option(global.tokens);
+		if (op_func == NULL)
+		{
+			dprintf(STDERR_FILENO, "L%u: unknown instruction %s\n",
+					global.line_num, global.tokens[0]);
+			free_cases(0), exit(EXIT_FAILURE);
+		}
+		op_func(&global.stack, global.line_num);
+		free_cases(2), global.line_num++, len = 0;
+	}
+	free_cases(1);
+	return (EXIT_SUCCESS);
 }
